5ba1369b9f0cb5d37a8f9aff133d36ce8d64b5f7
Laurice Minamihara <lauricejoie.m@gmail.com>
2024-06-07 16:15:46 +0900

    Remove favicon, comment out business page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'landing#index'
-  get 'business', to: 'business#index', as: :business
+  # get 'business', to: 'business#index', as: :business
   resources :users, only: [:new, :create]
 end
